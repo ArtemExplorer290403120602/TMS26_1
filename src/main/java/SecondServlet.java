258a4a3b86da381fc3b4e58c9f1cbcc4be7b700c
User <artem.zakrzhevski@mail.ru>
2024-01-18 17:13:39 +0300

    nothing1

---
+++ src/main/java/SecondServlet.java
@@ -0,0 +1,23 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@WebServlet("/my-page-two")
+public class SecondServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        Path htmlPath = Paths.get(getServletContext().getRealPath("/tms-two.html"));
+        Files.lines(htmlPath).forEach(out::println);
+    }
+}
